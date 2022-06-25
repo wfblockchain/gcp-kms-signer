e375945d4b1dac113b9914f9179c72180347be9b
Saveliy Yusufov <sy2685@columbia.edu>
2022-06-25 11:33:31 -0400

    Fix signing example

--- client/main.go
+++ client/main.go
@@ -3,11 +3,55 @@ package main
 import (
 	"context"
 	"log"
+	"math/big"
 	"time"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/wfblockchain/gcp-kms-signer-dlt/digestsigner"
 	pb "github.com/wfblockchain/gcp-kms-signer-dlt/proto"
 	"google.golang.org/grpc"
 )
+
+const pem = `-----BEGIN PUBLIC KEY-----
+MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEK+pIyZ5c51/TQQVfikG86gzOdzpRP4vf
+X0U93p2H9l6cw9acNdGoE9lVVPUp0/vMZ71ETrafJyWF7SwBcKg1GA==
+-----END PUBLIC KEY-----`
+const rpcURL = "https://cloudflare-eth.com"
+const sevaAddress = "0x4549f47920997A486e9986d2e3e4540230534A03"
+
+func genTestTx(ctx context.Context) (*types.Transaction, error) {
+	kmsPK, err := digestsigner.PemToPubkey(pem)
+	if err != nil {
+		return nil, err
+	}
+
+	kmsAddress := crypto.PubkeyToAddress(*kmsPK)
+
+	ethClient, err := ethclient.Dial(rpcURL)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce, err := ethClient.PendingNonceAt(ctx, kmsAddress)
+	if err != nil {
+		return nil, err
+	}
+
+	value := big.NewInt(100)  // in wei (1 eth = 10{^18} wei)
+	gasLimit := uint64(21000) // in units
+	gasPrice, err := ethClient.SuggestGasPrice(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	toAddress := common.HexToAddress(sevaAddress)
+	var data []byte
+	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+	return tx, nil
+}
 
 func main() {
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
@@ -20,9 +64,23 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	res, err := client.Sign(ctx, &pb.SignTxReq{Tx: tx})
+	tx, err := genTestTx(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Print(res.GetTx())
+	txBytes, err := tx.MarshalBinary()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := client.Sign(ctx, &pb.SignTxReq{Tx: txBytes})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tx.UnmarshalBinary(res.GetTx())
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print(tx.Hash())
 }
